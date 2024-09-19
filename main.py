dac733d879261ca1cbfa5084f49be009a13cc6e5
SAISHANMUKHASANGU <137365406+SAISHANMUKHASANGU@users.noreply.github.com>
2024-09-19 20:58:21 +0530

    main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 import smtplib
 
 URL="https://www.amazon.com/CanaKit-Raspberry-Starter-Kit-PRO/dp/B0CRSNCJ6Y/ref=sr_1_1?crid=1XR6FAJCAOYLL&dib=eyJ2IjoiMSJ9.TzIAlcap-JN7BzO8wHpHCV1kBEl4EFbvGKEftHseQ--2-6THr0K_AeFJrbC3iDT7YpbAewog2dwqqMvgjDqySA753TTGpDohOhjak6KODQQ2_pG4losfIgl10aeTvSvcCGk-MZKmnJvWaWuSsKLXYx5EV9-4b7HXouHrZur4EpGTiADJX5H_SIzmINoon3IGLV1naMO19U3yqd2AvPbkk9C2qqlHCtqbfSU4KGx-y0E.bYp0WogKeTVtAnW25X42lG3-Ib-hMdDev92fDXZCoDE&dib_tag=se&keywords=raspberry%2Bpi%2B5&qid=1726756996&sprefix=ras%2Caps%2C283&sr=8-1&th=1"
-sender = '20981a4248@raghuenggcollege.in'
-password = 'hkqakqpmwbfjvgys'
+sender = 'Your Email ID with which SMTP is Created'
+password = 'Password'
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
          "Accept-Language":"en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
          "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
